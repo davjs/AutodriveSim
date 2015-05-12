369451f51847b37575c6b088d7abeaca4bd6d7b9
Simeon Ivanov <simeon@ivanovsimeon.com>
2015-05-12 19:43:34 +0200

    Removed extra Driver module terminal.

--- autodrive.sh
+++ autodrive.sh
@@ -2,7 +2,7 @@
 (cd /opt/odv/bin \
 ; ./supercomponent --cid=111 --freq=20 > /dev/null 2>&1 & \
 ./cockpit --cid=111 > /dev/null 2>&1 & \
-xterm -e ./vehicle --cid=111 --freq=20 & \
+./vehicle --cid=111 --freq=20 > /dev/null 2>&1 & \
 ./camgen --cid=111 --freq=20 > /dev/null 2>&1 & \
 ./irus --cid=111 --freq=20 > /dev/null 2>&1 & \
 ./lanedetector --cid=111 --freq=20)
