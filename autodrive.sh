0035d436972a4205f889c430687b612c4bcff73f
AureFranky <au.re@live.com>
2015-05-16 17:57:30 +0200

    fixed autodrive bug

--- autodrive.sh
+++ autodrive.sh
@@ -1,9 +1,9 @@
 #! /bin/sh
-(cd /opt/odv/bin \
+(cd /opt/msv/bin \
 ; ./supercomponent --cid=111 --freq=20 > /dev/null 2>&1 & \
 ./cockpit --cid=111 > /dev/null 2>&1 & \
 ./vehicle --cid=111 --freq=20 > /dev/null 2>&1 & \
 ./camgen --cid=111 --freq=20 > /dev/null 2>&1 & \
 ./irus --cid=111 --freq=20 > /dev/null 2>&1 & \
-./lanedetector --cid=111 --freq=20)
+./AutodriveSim --cid=111 --freq=20)
 wait
